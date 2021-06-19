22e79f8aab7c0ea7fb7e3a5bc2559670fb376b82
Anshul <anshul.swarnkar1990@gmail.com>
2021-06-19 20:49:38 +0530

    Initial commit

---
+++ ClassDemo/FirstClass.py
@@ -0,0 +1,11 @@
+# First we are creating a class
+class A:
+    # This is class function
+    def welcome(self):
+        print("Welcome to class A")
+
+# To create any member of class, create object of that class
+obj=A()
+
+#  Call function of class by using object
+obj.welcome()
